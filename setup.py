dae6aa54edc3c0644a14eff6ff64798becbe1025
Eric Pascual <eric@pobot.org>
2015-09-26 12:49:28 +0200

    repackaging and explicit scripts replaced by entry points

--- setup.py
+++ setup.py
@@ -3,15 +3,23 @@
 
 from setuptools import setup, find_packages
 
-setup(name='nros_core',
-      version='1.0',
-      description='Core part of nROS framework',
-      license='LGPL',
-      author='Eric Pascual',
-      author_email='eric@pobot.org',
-      url='http://www.pobot.org',
-      download_url='https://github.com/Pobot/PyBot',
-      packages=find_packages("src"),
-      package_dir={'': 'src'},
-      include_package_date=True
-      )
+setup(
+    name='nros-core',
+    version='1.0.2',
+    description='Core part of nROS framework',
+    license='LGPL',
+    author='Eric Pascual',
+    author_email='eric@pobot.org',
+    url='http://www.pobot.org',
+    download_url='https://github.com/Pobot/PyBot',
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'nros-bus-start = nros.core.entry_points:nros_bus_start',
+            'nros-bus-stop = nros.core.entry_points:nros_bus_stop',
+            'nros-bus-status = nros.core.entry_points:nros_bus_status',
+            'nros-bus-config = nros.core.entry_points:nros_bus_config',
+        ]
+    }
+)
