1f4ab7c7463c4116dd640fbbdfbab75d111a2138
Eric Pascual <eric@pobot.org>
2016-09-10 11:45:06 +0200

    updated for new systemd helper version

--- src/nros/core/setup/systemd.py
+++ src/nros/core/setup/systemd.py
@@ -11,7 +11,7 @@ SERVICE_NAME = 'nros-bus'
 
 def install_service():
     try:
-        if not SystemdSetupHelper(SERVICE_NAME).install_service():
+        if not SystemdSetupHelper(SERVICE_NAME, __name__).install_service():
             print("already installed")
     except RuntimeError as e:
         sys.exit("ERROR: %s" % e)
@@ -19,7 +19,7 @@ def install_service():
 
 def remove_service():
     try:
-        if not SystemdSetupHelper(SERVICE_NAME).remove_service():
+        if not SystemdSetupHelper(SERVICE_NAME, __name__).remove_service():
             print("not installed")
     except RuntimeError as e:
         sys.exit("ERROR: %s" % e)
